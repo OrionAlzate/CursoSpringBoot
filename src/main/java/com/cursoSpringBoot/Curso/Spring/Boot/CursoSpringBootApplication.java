421e16d4260ba79ff377100d2a2d03a4c4d4b827
OrionAlzate <orion_alzate@hotmail.com>
2023-02-28 17:06:49 -0500

    primer commit spring boot

---
+++ src/main/java/com/cursoSpringBoot/Curso/Spring/Boot/CursoSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.cursoSpringBoot.Curso.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+		// System.out.println("Hola mundo");
+
+
+	}
+
+}
---
+++ src/main/java/com/cursoSpringBoot/Curso/Spring/Boot/controller/userController.java
@@ -0,0 +1,7 @@
+package com.cursoSpringBoot.Curso.Spring.Boot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+}
